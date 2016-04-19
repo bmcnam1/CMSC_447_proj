4772effb49ac43d4941582ec586376ac5c21eb13
mlanden <mlanden@umbc.edu>
2016-04-18 22:06:20 -0400

    line graoh

--- PieChart.php
+++ PieChart.php
@@ -8,10 +8,6 @@
 		 google.charts.setOnLoadCallback(drawChart); 
 	  }
       
-	var graph_data;  
-	var data;
-	var chart;
-	var options;
 	function canAccessGoogleVisualization() 
 {
     if ((typeof google === 'undefined') || (typeof google.visualization === 'undefined')) {
@@ -23,110 +19,152 @@
 }
 	//get initial data
     function drawChart() {
-		  
+		var url = window.location.href;
+		var params = url.substring(url.indexOf("?"));
 		var jsonData = $.ajax({
-          url: "Data.php",
+          url: "data.php",
 		  contentType: "application/json",
          
           async: false,
 		  success: function(data){
-            
-                graph_data = data;
-                computeChart(graph_data);
-		    
+            drawPieChart(data);
+            drawLineGraph(data);
 		  }
         });
 	}		
+
+	function drawLineGraph(graph_data){
+		graph_data = graph_data.replace("In data.php", "");
+		graph_data = graph_data.substring(0, graph_data.indexOf(']')+1);
+		var JsonArray = JSON.parse(graph_data);
+		var data = [];
+		var types = []
+		for(var i in JsonArray){
+			var type = JsonArray[i]['crimeType'];
+			if(types.indexOf(type) == -1){
+				types.push(type);
+			}
+
+			
+		}
+		//make sure each date has every type
+		for(var i in JsonArray){
+			var type = JsonArray[i]['crimeType'];
+			var dateStr = JsonArray[i]['crimeDateTime'];
+			var day = dateStr.substring(0,dateStr.indexOf(" "));
+			if(!data.hasOwnProperty(day)){
+				data[day] = [];
+				for(var typ in types){
+					data[day][types[typ]] = 0;
+				}
+			}
+			data[day][type]++;
+		}
+
+		var table  = new google.visualization.DataTable();
+		table.addColumn('string','Day');
+		for(var i in types){
+			table.addColumn('number', types[i]);
+		}
+
+		for(var date in data){
+			var row = [date];
+			for (var count in data[date]){
+				row.push(data[date][count]);
+			}
+			table.addRow(row);
+		}
+
+		var options = {'title':'line Chart of Baltimoere Crimes',
+    					'width':1000,
+    					'height':900};
+    	var chart = new google.visualization.LineChart(document.getElementById("linechart"));
+    	chart.draw(table, options);
+	}
 	//for intital chart setup
-	function computeChart(graph_data){
-		alert(graph_data);
+	function drawPieChart(graph_data){
 		///Clean up the JSON string from Data.php
 		graph_data = graph_data.replace("In data.php", "");
 		graph_data = graph_data.substring(0, graph_data.indexOf(']')+1);
-		
-	
-		 var JSONObject = JSON.parse(graph_data);
-		 console.log(JSONObject);
-		 
-		    var array = JSONObject;
-			var counts = {};
-			for(var i in array){
-				var nam = array[i]['crimeType'];
-				if(counts.hasOwnProperty(nam)){
-					counts[nam] ++;
-				}else{
-					counts[nam] = 1;
-				}
+		var JsonArray = JSON.parse(graph_data);
+		var counts = {};
+		for(var i in JsonArray){
+			var nam = JsonArray[i]['crimeType'];
+			if(counts.hasOwnProperty(nam)){
+				counts[nam] ++;
+			}else{
+				counts[nam] = 1;
 			}
-			data = new google.visualization.DataTable();
-			data.addColumn('string', 'Crime type');
-        	data.addColumn('number', 'count');
-        	for(var prop in counts){
-        		data.addRow([prop,counts[prop]]);
-        	}
-        	options = {'title':'Pie Chart of Baltimoere Crimes',
-        					'width':800,
-        					'height':700};
-        	chart = new google.visualization.PieChart(document.getElementById('piechart'));
-			//alert("drawing the chart");
-        	chart.draw(data, options);
+		}
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Crime type');
+    	data.addColumn('number', 'count');
+    	for(var prop in counts){
+    		data.addRow([prop,counts[prop]]);
+    	}
+    	var options = {'title':'Pie Chart of Baltimoere Crimes',
+    					'width':800,
+    					'height':700};
+    	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+		//alert("drawing the chart");
+    	chart.draw(data, options);
 
     }
 	
 	//redraw the chart with updated data - triggered by onClick() event
 	function redraw(){
-		//Get the inner.HTML of the hidden 'div' and split on ','
-		var newData = document.getElementById('dataDiv').innerHTML.split(",");
-		//
-		for(i = 0; i < newData.length; i++){
-			//try to clean up data if null
-			if (typeof newData[i] == 'undefined'){
-				//alert("It was undefined"+newData[i]);
-				newData[i] = "";
-			}
-		}
-		//TODO: newData[2] or 'streetname' is always undefined -use nothing for now.
-		var jsonData = $.ajax({
-          url: "Data.php",
-          data: {district:newData[0], 
-		         neighborhood:newData[1],
-				 streetname:"" //newData[2]
-				},
-		  type: "POST",
-          async: false
+	// 	//Get the inner.HTML of the hidden 'div' and split on ','
+	// 	var newData = document.getElementById('dataDiv').innerHTML.split(",");
+	// 	//
+	// 	for(i = 0; i < newData.length; i++){
+	// 		//try to clean up data if null
+	// 		if (typeof newData[i] == 'undefined'){
+	// 			//alert("It was undefined"+newData[i]);
+	// 			newData[i] = "";
+	// 		}
+	// 	}
+	// 	//TODO: newData[2] or 'streetname' is always undefined -use nothing for now.
+	// 	var jsonData = $.ajax({
+ //          url: "Data.php",
+ //          data: {district:newData[0], 
+	// 	         neighborhood:newData[1],
+	// 			 streetname:"" //newData[2]
+	// 			},
+	// 	  type: "POST",
+ //          async: false
 		  
-        }).responseText;
-		//alert("new JsonData = "+jsonData);
-		graph_data = jsonData;
+ //        }).responseText;
+	// 	//alert("new JsonData = "+jsonData);
+	// 	graph_data = jsonData;
 		
-		graph_data = graph_data.substring(0, graph_data.indexOf(']')+1);
+	// 	graph_data = graph_data.substring(0, graph_data.indexOf(']')+1);
 		
 	
-		 var JSONObject = JSON.parse(graph_data);
-		 console.log(JSONObject);
+	// 	 var JSONObject = JSON.parse(graph_data);
+	// 	 console.log(JSONObject);
 		 
-		    var array = JSONObject;
-			var counts = {};
-			for(var i in array){
-				var nam = array[i]['crimeType'];
-				if(counts.hasOwnProperty(nam)){
-					counts[nam] ++;
-				}else{
-					counts[nam] = 1;
-				}
-			}
-			data = new google.visualization.DataTable();
-			data.addColumn('string', 'Crime type');
-        	data.addColumn('number', 'count');
-        	for(var prop in counts){
-        		data.addRow([prop,counts[prop]]);
-        	}
-			options = {'title':'Pie Chart of Baltimoere Crimes',
-        					'width':800,
-        					'height':700};
+	// 	    var array = JSONObject;
+	// 		var counts = {};
+	// 		for(var i in array){
+	// 			var nam = array[i]['crimeType'];
+	// 			if(counts.hasOwnProperty(nam)){
+	// 				counts[nam] ++;
+	// 			}else{
+	// 				counts[nam] = 1;
+	// 			}
+	// 		}
+	// 		data = new google.visualization.DataTable();
+	// 		data.addColumn('string', 'Crime type');
+ //        	data.addColumn('number', 'count');
+ //        	for(var prop in counts){
+ //        		data.addRow([prop,counts[prop]]);
+ //        	}
+	// 		options = {'title':'Pie Chart of Baltimoere Crimes',
+ //        					'width':800,
+ //        					'height':700};
         	
-			//alert("drawing the chart");
-        	chart.draw(data, options);
+	// 		//alert("drawing the chart");
+ //        	chart.draw(data, options);
 		
 	}
 	  
@@ -136,5 +174,6 @@
   <body>
 	<div id="dataDiv" onclick='redraw();' hidden></div>
     <div id="piechart" style="width: 900px; height: 500px;"></div>
+    <div id="linechart" style="width: 900px; height: 500px;"></div>
   </body>
 </html>
